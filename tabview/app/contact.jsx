dbe1c90158001c499d9da194af87ec6825f4e245
Pranav-hub1326 <Pranav1326100@gmail.com>
2025-11-19 11:11:24 +0530

    HLCI

--- tabview/app/contact.jsx
+++ tabview/app/contact.jsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function contact() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>contact</Text>
     </View>
   )
 }
--- tabview/app/project.jsx
+++ tabview/app/project.jsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Home() {
+export default function project() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>project</Text>
     </View>
   )
 }
